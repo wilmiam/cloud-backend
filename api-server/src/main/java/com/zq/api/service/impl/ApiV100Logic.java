91f8cbaa421c1a63da7301ad04136aac8df035b4
ywm <roxywm@163.com>
2021-04-14 15:34:28 +0800

    1.0.0

--- api-server/src/main/java/com/zq/api/service/impl/ApiV100Logic.java
+++ api-server/src/main/java/com/zq/api/service/impl/ApiV100Logic.java
@@ -1,16 +1,19 @@
 package com.zq.api.service.impl;
 
+import com.zq.api.feign.CmsFeign;
 import com.zq.api.form.ApiForm;
 import com.zq.api.form.ApiResp;
 import com.zq.api.service.IApiLogic;
 import com.zq.api.utils.ApiUtils;
 import com.zq.common.vo.ResultVo;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class ApiV100Logic extends BaseApiLogic implements IApiLogic {
+
+    @Autowired
+    private CmsFeign cmsFeign;
 
     /**
      * 测试连接
