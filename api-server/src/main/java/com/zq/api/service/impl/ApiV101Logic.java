1f52e6864e5e66ca0c4d7abd2c5476f976996206
ywm <roxywm@163.com>
2021-04-14 15:27:52 +0800

    1.0.0

--- api-server/src/main/java/com/zq/api/service/impl/ApiV101Logic.java
+++ api-server/src/main/java/com/zq/api/service/impl/ApiV101Logic.java
@@ -1,13 +1,9 @@
 package com.zq.api.service.impl;
 
-import com.zq.api.feign.CmsFeign;
 import com.zq.api.service.IApiLogic;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApiV101Logic extends ApiV100Logic implements IApiLogic {
 
-    public ApiV101Logic(CmsFeign cmsFeign) {
-        super(cmsFeign);
-    }
 }
