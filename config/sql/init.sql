5050e639a814f37b6d4bba2aee80c56bdb58e6b9
ywm <roxywm@163.com>
2021-04-18 09:14:58 +0800

    1.0.0

--- config/sql/init.sql
+++ config/sql/init.sql
@@ -38,26 +38,13 @@ CREATE TABLE `sys_config`  (
 -- ----------------------------
 -- Records of sys_config
 -- ----------------------------
-INSERT INTO `sys_config` VALUES (1, '系统参数', 'systemParam', '0', '0', 10, '2016-12-17 22:32:35', 1, '2016-12-17 22:32:35', 1);
-INSERT INTO `sys_config` VALUES (2, 'API参数', 'apiParam', '0', '0', 11, '2016-12-17 22:33:41', 1, '2016-12-17 22:33:41', 1);
-INSERT INTO `sys_config` VALUES (4, '版权', 'copyright', '©广西杉网科技有限公司 版权所有', '1', 10, '2016-12-17 23:07:21', 1, '2016-12-17 23:07:21', 1);
-INSERT INTO `sys_config` VALUES (5, 'API是否开启', 'API.FLAG', 'true', '2', 110, '2016-12-17 23:12:26', 1, '2016-12-17 23:12:26', 1);
-INSERT INTO `sys_config` VALUES (6, 'ip黑名单，逗号分隔', 'API.IP.BLACK', '127.0.0.122,localhost22', '2', 111, '2016-12-17 23:16:29', 1, '2016-12-17 23:16:29', 1);
-INSERT INTO `sys_config` VALUES (7, '支持的版本，逗号分隔', 'API.VERSIONS', '1.0.0,1.0.1', '2', 112, '2016-12-17 23:17:00', 1, '2016-12-17 23:17:00', 1);
-INSERT INTO `sys_config` VALUES (8, '登陆验证是否开启', 'API.LOGIN.VALID', 'false', '2', 114, '2016-12-17 23:17:23', 1, '2016-12-17 23:17:23', 1);
-INSERT INTO `sys_config` VALUES (9, '站点参数', 'siteParam', '0', '0', 12, '2016-12-31 16:27:12', 1, '2016-12-31 16:27:12', 1);
-INSERT INTO `sys_config` VALUES (10, '多站点标示', 'SITE.MULTI.FLAG', 'false', '9', 211, '2016-12-31 16:28:02', 1, '2016-12-31 16:28:02', 1);
-INSERT INTO `sys_config` VALUES (11, '站点根目录', 'SITE.TEMPLATE.PATH', '/template/', '9', 212, '2016-12-31 16:28:43', 1, '2016-12-31 16:28:43', 1);
-INSERT INTO `sys_config` VALUES (12, 'Session站点列表', 'SITE.SESSION.SITES', 'sites', '9', 213, '2016-12-31 16:30:17', 1, '2016-12-31 16:30:17', 1);
-INSERT INTO `sys_config` VALUES (13, 'Session站点', 'SITE.SESSION.SITE', 'site', '9', 214, '2016-12-31 16:30:38', 1, '2016-12-31 16:30:38', 1);
-INSERT INTO `sys_config` VALUES (14, 'API参数加密是否开启', 'API.PARAM.ENCRYPT', 'false', '2', 115, '2017-03-17 23:17:23', 1, '2017-03-17 23:17:23', 1);
-INSERT INTO `sys_config` VALUES (15, '文件备份参数', 'backupParam', '0', '0', 10, '2017-04-05 03:49:21', 1, '2017-04-05 03:49:21', 1);
-INSERT INTO `sys_config` VALUES (16, '文件备份类型名称', 'backup.name', 'filemanger', '15', 10, '2017-04-05 03:50:12', 1, '2017-04-05 03:50:12', 1);
-INSERT INTO `sys_config` VALUES (17, '文件系统备份路径', 'backup.filemanger.path', 'D:\\test', '15', 10, '2017-04-05 03:50:42', 1, '2017-04-05 03:50:42', 1);
-INSERT INTO `sys_config` VALUES (18, '阿里云存储bucketname', 'backup.oss.bucketname', 'jflyfox', '15', 10, '2017-04-05 22:58:05', 1, '2017-04-05 22:58:05', 1);
-INSERT INTO `sys_config` VALUES (19, '阿里云存储endpoint', 'backup.oss.endpoint', 'http://oss-cn-beijing.aliyuncs.com', '15', 10, '2017-04-05 22:59:01', 1, '2017-04-05 22:59:01', 1);
-INSERT INTO `sys_config` VALUES (20, '阿里云存储ID', 'backup.oss.id', '', '15', 10, '2017-04-05 22:59:24', 1, '2017-04-05 22:59:24', 1);
-INSERT INTO `sys_config` VALUES (21, '阿里云存储KEY', 'backup.oss.key', '', '15', 10, '2017-04-05 22:59:51', 1, '2017-04-05 22:59:51', 1);
+INSERT INTO `sys_config` VALUES (1, 'API是否开启', 'API.FLAG', 'true', 'bool', 110, '2016-12-17 23:12:26', 1, '2016-12-17 23:12:26', 1);
+INSERT INTO `sys_config` VALUES (2, 'ip黑名单，逗号分隔', 'API.IP.BLACK', '127.0.0.122,localhost22', 'txt', 111, '2016-12-17 23:16:29', 1, '2016-12-17 23:16:29', 1);
+INSERT INTO `sys_config` VALUES (3, '支持的版本，逗号分隔', 'API.VERSIONS', '1.0.0,1.0.1', 'txt', 112, '2016-12-17 23:17:00', 1, '2016-12-17 23:17:00', 1);
+INSERT INTO `sys_config` VALUES (4, '登陆验证是否开启', 'API.LOGIN.VALID', 'false', 'bool', 114, '2016-12-17 23:17:23', 1, '2016-12-17 23:17:23', 1);
+INSERT INTO `sys_config` VALUES (5, 'API参数加密是否开启', 'API.PARAM.ENCRYPT', 'false', 'bool', 115, '2017-03-17 23:17:23', 1, '2017-03-17 23:17:23', 1);
+INSERT INTO `sys_config` VALUES (6, '腾讯云秘钥ID', 'CLOUD.SECRET.ID', 'AKIDwi1CB9h9HDex4yaahdbVHzVXN3XpqzEH', 'txt', 10, '2017-04-05 22:59:24', 1, '2017-04-05 22:59:24', 1);
+INSERT INTO `sys_config` VALUES (7, '腾讯云秘钥KEY', 'CLOUD.SECRET.KEY', 'SghR2ECxh5D3rsmiTH2XoqrbXGl5A0a0', 'txt', 10, '2017-04-05 22:59:51', 1, '2017-04-05 22:59:51', 1);
 
 -- ----------------------------
 -- Table structure for t_api_log
