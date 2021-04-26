4f29e256aa08637645cf7d23650ec4a9361f74fe
ywm <roxywm@163.com>
2021-04-26 17:27:35 +0800

    1.0.0

---
+++ api-server/src/main/java/com/zq/api/config/FeignConfig.java
@@ -0,0 +1,39 @@
+package com.zq.api.config;
+
+import com.zq.common.http.HttpRequestUtils;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+@Configuration
+public class FeignConfig {
+
+    /**
+     * 解决fein远程调用丢失请求头
+     *
+     * @return
+     */
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate template) {
+                HttpServletRequest request = HttpRequestUtils.getRequest();
+                template.header("X-App-Token", request.getParameter("token"));
+                Enumeration<String> headerNames = request.getHeaderNames();
+                if (headerNames != null) {
+                    while (headerNames.hasMoreElements()) {
+                        String name = headerNames.nextElement();
+                        String values = request.getHeader(name);
+                        template.header(name, values);
+                    }
+                }
+            }
+        };
+    }
+
+}
--- api-server/src/main/java/com/zq/api/feign/CmsFeign.java
+++ api-server/src/main/java/com/zq/api/feign/CmsFeign.java
@@ -1,5 +1,6 @@
 package com.zq.api.feign;
 
+import com.zq.api.config.FeignConfig;
 import com.zq.common.vo.ResultVo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
-@FeignClient(name = "CMS-SERVER")  //指定调用哪个微服务
+@FeignClient(name = "CMS-SERVER", configuration = FeignConfig.class)  //指定调用哪个微服务
 @RequestMapping("/cms")
 public interface CmsFeign {
 
