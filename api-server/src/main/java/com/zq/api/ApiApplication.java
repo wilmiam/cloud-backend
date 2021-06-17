dac5549353147578a984794999d9aee8f940246e
ywm <roxywm@163.com>
2021-06-17 15:07:34 +0800

    1.0.0

--- api-server/src/main/java/com/zq/api/ApiApplication.java
+++ api-server/src/main/java/com/zq/api/ApiApplication.java
@@ -1,11 +1,9 @@
 package com.zq.api;
 
 import com.zq.common.config.base.SpringContextHolder;
-import com.zq.common.config.security.SecurityProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -22,12 +20,6 @@ public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
-    }
-
-    @Bean
-    @ConfigurationProperties(prefix = "jwt")
-    public SecurityProperties securityProperties() {
-        return new SecurityProperties();
     }
 
     @Bean
--- cms-server/src/main/java/com/zq/cms/CmsApplication.java
+++ cms-server/src/main/java/com/zq/cms/CmsApplication.java
@@ -1,12 +1,9 @@
 package com.zq.cms;
 
-import com.zq.common.config.security.SecurityProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -17,12 +14,6 @@ public class CmsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CmsApplication.class, args);
-    }
-
-    @Bean
-    @ConfigurationProperties(prefix = "jwt")
-    public SecurityProperties securityProperties() {
-        return new SecurityProperties();
     }
 
 }
